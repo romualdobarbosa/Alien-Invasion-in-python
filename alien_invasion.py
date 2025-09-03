12fac9c769023d6999280d8c81e5750aaa7832cb
romualdobarbosa <romualdoferreirabarbosa@gmail.com>
2025-09-03 20:31:05 -0300

    :zap: <build> Make classes: ship, settings and Alien

---
+++ alien_invasion.py
@@ -0,0 +1,35 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+  def __init__(self):
+    """Start the game methods"""
+    pygame.init() 
+    self.clock = pygame.time.Clock()
+    self.settings = Settings()
+    
+    """Create the screen"""
+    self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    """Create ship with ship class"""
+    self.ship = Ship(self)
+
+
+
+  def run_game(self):
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
+        
+      self.screen.fill(self.settings.bg_color)  
+      self.ship.blitme()
+      pygame.display.flip()
+      self.clock.tick(60) 
+
+
+if __name__ == '__main__':
+  ai = AlienInvasion()
+  ai.run_game()
